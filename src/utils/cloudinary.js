a7d881722250b37ae5df02237e64f5704d92a776
manish50kumar <manish5060773@gmail.com>
2024-04-11 07:55:32 +0530

    upload image or video on cloudinary functionality

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -7,4 +8,25 @@ cloudinary.config({
     secure:true    
 })
 
+// upload image or video
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // upload file on cloudinary
+        const response = cloudinary.uploader.upload(localFilePath, {
+            resource_type: auto
+        })
+        // file successful upload on cloud
+        console.log("upload file on cloudinary url : ", response.url);
+
+        // delete file from local file
+        fs.unlinkSync(localFilePath);
+        return response;
+
+    } catch (error) {
+        console.log("Error in upload data to cloudinary : ", error);
+        fs.unlinkSync(localFilePath); // delete file from local file
+        return null;
+    }
+}
 

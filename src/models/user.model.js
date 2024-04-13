3d86834bb82818917ce0814dd229b20b444d8c32
manish50kumar <manish5060773@gmail.com>
2024-04-13 14:36:54 +0530

    bcrypt and hash password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username: {
@@ -44,5 +45,12 @@ const userSchema = new Schema({
         type:String,
     }
 })
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 export const User = mongoose.model("User", userSchema);

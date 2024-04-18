141f5ff9a5c8dd950fb47f291d52ecb3fdfed185
manish50kumar <manish5060773@gmail.com>
2024-04-18 22:53:16 +0530

    register user post route created

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,26 @@ import { Router } from "express";
 import { upload } from "../middlewares/multer.js";
 import { vefifyJWT } from "../middlewares/auth.js";
 
+import {
+    registerUser,
+    loginUser
+} from "../controllers/user.controller.js";
+
 const router = Router();
+
+router.post(
+    "/register",
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
 
 export default router;
